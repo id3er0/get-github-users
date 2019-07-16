4fb68e75f31c007d6a49930c0641f8dc4bed6c64
ID0 <id0@posteo.net>
2019-07-16 19:06:01 +0300

    Change API method to get users.

--- src/index.js
+++ src/index.js
@@ -3,21 +3,17 @@ const axios = require('axios');
 const host = 'https://api.github.com';
 const paths = {
   LIMIT: '/rate_limit',
-  SEARCH: '/search/users',
+  SEARCH: '/users',
 };
 const API = {};
 for (const [key, value] of Object.entries(paths)) {
   API[key] = host + value;
 }
 
-// Save data in memory to show counting in console.
-let savedData = [];
+const USERS_IN_REQUEST = 100;
 
-// Github accepts 100 here but we have to limit this param for this demo, because of another API limitation:
-// > Only the first 1000 search results are available
-const USERS_IN_REQUEST = 5;
-
-let page = 1;
+// TODO: This demo does not count saved to the database users on the first run.
+let since = 0;
 
 let limit = {
   remaining: 0,
@@ -58,7 +54,7 @@ const getLimit = async () => {
       // limit,
       remaining,
       reset,
-    } = response.data.resources.search;
+    } = response.data.resources.core;
 
     limit = {
       remaining,
@@ -87,21 +83,18 @@ const getSearchResult = async () => {
   try {
     const response = await axios.get(API.SEARCH, {
       params: {
-        q: 'type:user',
-        // Sort users
-        sort: 'joined',
-        order: 'asc',
-        page,
+        since,
         per_page: USERS_IN_REQUEST,
       },
     });
-    const {items} = response.data;
+    const items = response.data;
 
     if (Array.isArray(items) && items.length > 0) {
       // Add one page.
       // In this release we do not cover situations,
       // when API sends less than `USERS_IN_REQUEST` users in array.
-      ++page;
+      since += items.length;
+      console.log('getSearchResult - since:', since);
       return items;
     } else {
       return [];
@@ -123,9 +116,7 @@ const repeatSearch = async () => {
   for (const i of Array.from({length: limit.remaining})) {
     const addedUsers = await getSearchResult();
     insertDocuments(addedUsers);
-    // Just for counting in console.
-    savedData = savedData.concat(addedUsers);
-    console.log('repeatSearch - savedData.length:', savedData.length);
+    console.log('repeatSearch - since:', since);
   }
 };
 
